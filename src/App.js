b24e92784560dd80ad1590b6318dd31a0067c63d
Sarah Rose <sarahrosesmr@gmail.com>
2021-01-23 19:52:18 -0500

    add scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import Stitcher from './Stitcher'
-import { BrowserRouter as Router } from 'react-router-dom'
+// import React from 'react';
+// import './App.css';
+// import Stitcher from './Stitcher'
+// import { BrowserRouter as Router } from 'react-router-dom'
 
-function App() {
-  return (
-    <Router>
-        <Stitcher />
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//         <Stitcher />
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
